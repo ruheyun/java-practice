045c6b0fe8c5de5df47a13fa629aa1defcbca388
ruheyun <1654033661@qq.com>
2023-02-08 15:45:33 +0800

    day01

---
+++ java-demo01/src/cn/ruhe/study/Day01.java
@@ -0,0 +1,2 @@
+package cn.ruhe.study;public class Day01 {
+}
--- java-demo01/src/cn/ruhe/study/test.java
+++
@@ -1,7 +0,0 @@
-package cn.ruhe.study;
-
-public class test {
-    public static void main(String[] args) {
-        System.out.println("hello world");
-    }
-}
